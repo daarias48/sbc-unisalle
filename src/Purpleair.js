cbb629a460a7d525a1882dbcd0dcf9ff213fc6ba
andresflorez16 <andresflorez.dev@gmail.com>
2021-12-22 14:22:59 -0500

    plantower1

--- src/Purpleair.js
+++ src/Purpleair.js
@@ -5,11 +5,13 @@ const purpleairSensor = (data) => {
     const date = dateFormat[0]
     const hourFormat = dateFormat[1].split(':')
     const hour = `${hourFormat[0]}:${hourFormat[1]}`
-    const tempe = parseFloat(data.temp_f).toFixed(0)
     const rh = parseFloat(data.humidity).toFixed(0)
     const pm1 = parseFloat(data.pm1_0_atm).toFixed(2)
     const pm10 = parseFloat(data.pm10_0_atm).toFixed(2)
     const pm25 = parseFloat(data.pm2_5_atm).toFixed(2)
+    
+    const tempF = parseFloat(data.temp_f).toFixed(0)
+    const tempe = ((tempF - 32) * (5 / 9)).toFixed(0)
 
     return Sensor = {
         id: data.LastSeen,
--- src/public/js/plantower.js
+++ src/public/js/plantower.js
@@ -1,55 +1,82 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
 import { getDatabase, ref, onChildAdded, get, child, limitToLast, query, onValue } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDeawHKlf1NBvPDIUer0sYDxn7WrIIL3ag",
-    authDomain: "mysensorinfo.firebaseapp.com",
-    databaseURL: "https://mysensorinfo-default-rtdb.firebaseio.com",
-    projectId: "mysensorinfo",
-    storageBucket: "mysensorinfo.appspot.com",
-    messagingSenderId: "72274332118",
-    appId: "1:72274332118:web:b0ee741dcfe8604fa13c77",
-    measurementId: "G-RZMGVP8RQT"
+// var ctx = document.getElementById('myChartModulair').getContext('2d');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTHKTT0EVfyPa1KLNIFz0_8zuyfT8OolQ",
+    authDomain: "mysensorappuwu.firebaseapp.com",
+    databaseURL: "https://mysensorappuwu-default-rtdb.firebaseio.com",
+    projectId: "mysensorappuwu",
+    storageBucket: "mysensorappuwu.appspot.com",
+    messagingSenderId: "328704721464",
+    appId: "1:328704721464:web:e9c4439a96d62b41121ccd",
+    measurementId: "G-QLTKSRSBWZ"
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig, "daniAPP");
+const dbRef = getDatabase(app);
 
-const tempTower1 = document.getElementById('tempTower1');
-const pm1Tower1 = document.getElementById('pm1Tower1');
-const pm25Tower1 = document.getElementById('pm25Tower1');
-const pm10Tower1 = document.getElementById('pm10Tower1');
-const humTower1 = document.getElementById('humTower1');
-const dateTower1 = document.getElementById('dateTower1');
+const temp = document.getElementById('temp');
+const rh = document.getElementById('rh');
+const pm1_1 = document.getElementById('pm1');
+const pm10_1 = document.getElementById('pm10');
+const pm25_1 = document.getElementById('pm25');
+const date = document.getElementById('date');
+const hour = document.getElementById('hour');
 
-const tempTower2 = document.getElementById('tempTower2');
-const pm1Tower2 = document.getElementById('pm1Tower2');
-const pm25Tower2 = document.getElementById('pm25Tower2');
-const pm10Tower2 = document.getElementById('pm10Tower2');
-const humTower2 = document.getElementById('humTower2');
-const dateTower2 = document.getElementById('dateTower2');
+const deviceName = document.getElementById('deviceName');
+const device = document.getElementById('device');
+const storage = document.getElementById('storage');
+const comunication = document.getElementById('comunication');
+const maker = document.getElementById('maker');
 
-const dbRef = getDatabase();
+const macPlantower1 = '10:52:1C:5E:18:44'
+const dates = []
+const rhs = []
+const temps = []
+const pm1 = []
+const pm10 = []
+const pm25 = []
 
-const referenceTower1 = ref(dbRef, 'sensoresbajocosto/plantower1')
-onValue(referenceTower1, (snap) => {
-    let tower1 = snap.val()
-    tempTower1.innerHTML = tower1.temperatura
-    humTower1.innerHTML = tower1.humedad
-    dateTower1.innerHTML = tower1.fecha
-    pm1Tower1.innerHTML = tower1.PM10
-    pm25Tower1.innerHTML = tower1.PM25
-    pm10Tower1.innerHTML = tower1.PM100
+const hours = []
+const datesFormat = []
+
+const myRef = ref(dbRef, 'sensoresbajocosto')
+get(child(myRef, `plantower/${macPlantower1}`))
+.then(data => {
+    if(data.exists()){
+        const dato = data.val()
+        for(let key in dato) {
+            dates.push(key)
+            rhs.push(dato[key].humedad)
+            temps.push(dato[key].temperatura)
+            pm1.push(dato[key].pm1)
+            pm10.push(dato[key].pm10)
+            pm25.push(dato[key].pm2)
+        }
+        dates.map(el => {
+            const splited = el.split(':')
+            const f = `${splited[2]}/${splited[1]}/${splited[0]}`
+            const h = `${splited[3]}:${splited[4]}`
+            hours.push(h)
+            datesFormat.push(f)
+        })
+
+        deviceName.innerHTML = "Plantower 7003"
+        device.innerHTML = macPlantower1
+        storage.innerHTML = "110 VAC"
+        comunication.innerHTML = "Wifi"
+        maker.innerHTML = "AVARC"
+
+        temp.innerHTML = `${temps[temps.length - 1].toFixed(0)} °C`
+        hour.innerHTML = `${hours[hours.length - 1]}`
+        date.innerHTML = `${datesFormat[datesFormat.length - 1]}`
+        console.log(typeof temps[0])
+
+    }
+    else console.log('no existe')
 })
-
-const referenceTower2 = ref(dbRef, 'sensoresbajocosto/plantower2')
-onValue(referenceTower2, (snap) => {
-    let tower2 = snap.val()
-    tempTower2.innerHTML = tower2.temperatura
-    humTower2.innerHTML = tower2.humedad
-    dateTower2.innerHTML = tower2.fecha
-    pm1Tower2.innerHTML = tower2.PM10
-    pm25Tower2.innerHTML = tower2.PM25
-    pm10Tower2.innerHTML = tower2.PM100
-})
+.catch(err => console.log(err))
 
